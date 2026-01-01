6adb7d4406bb386ca5ce57f6c88d153bd65a7520
Bill Westfall <billwestfall@yahoo.com>
2025-12-31 22:01:46 -0800

    Create music_randb.rb

---
+++ alda/music_randb.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby
+
+print "piano: (tempo 5) "
+  $i = 0
+    while $i < 640 do
+      print [ :o2, :o3, :o4, :o5, :o6, :o7, :o8].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"" ].sample
+      print [ :"/", :"" ].sample
+      print " "
+      $i +=1
+    end
+print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+puts ""
+puts ""
+print "piano: (tempo 5) "
+  $i = 0
+    while $i < 640 do
+      print [ :o2, :o3, :o4].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"" ].sample
+      print [ :"/", :"" ].sample
+      print " "
+      $i +=1
+    end
+print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+puts ""
+puts ""
+print "piano: (tempo 5) "
+  $i = 0
+    while $i < 640 do
+      print [ :o2, :o3, :o4, :o5].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"" ].sample
+      print [ :"" ].sample
+      print " "
+      $i +=1
+    end
